066ddc73320bd6bef5f1ef545b191872ac7fb822
amanda <amandamarqueslinger@gmail.com>
2026-08-12 17:10:39 -0300

    commit init

---
+++ src/app/pages/login-page/login-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {FooterComponent} from '../../components/footer-component/footer-component';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [FooterComponent],
+  templateUrl: '../../../../public/assets/login-page.html',
+  styleUrl: './login-page.scss',
+})
+export class LoginPage {
+
+}
