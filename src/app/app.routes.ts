5b76518bd611e6e8f9d793e6db7b0f1792d2306b
amanda <amandamarqueslinger@gmail.com>
2026-08-13 16:26:58 -0300

    implementando rota para home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { InitialPage } from './pages/initial-page/initial-page';
 import { LoginPage } from './pages/login-page/login-page';
+import {HomePage} from './pages/home-page/home-page';
 
 export const routes: Routes = [
   { path: '', component: InitialPage },
+  { path: 'home', component: HomePage },
   { path: 'login', component: LoginPage },
   { path: '**', redirectTo: '' }
 ];
