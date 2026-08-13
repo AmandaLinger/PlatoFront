f232bac3ddf4f45e9316a16b15f5ff2afd13a373
amanda <amandamarqueslinger@gmail.com>
2026-08-13 16:23:40 -0300

    ajustando layout

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {FooterComponent} from '../../components/footer-component/footer-component';
 import {BtnOrange} from '../../components/btn-orange/btn-orange';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [FooterComponent, BtnOrange],
+  imports: [FooterComponent, BtnOrange, RouterLink],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
