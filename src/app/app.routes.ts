e5acd2de5e146f9e374ec3cef9f3d0b7b4bcff19
amanda <amandamarqueslinger@gmail.com>
2026-08-13 14:33:49 -0300

    implementando rota na página inicial e ajuste de layout final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InitialPage } from './pages/initial-page/initial-page';
+import { LoginPage } from './pages/login-page/login-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InitialPage },
+  { path: 'login', component: LoginPage },
+  { path: '**', redirectTo: '' }
+];
