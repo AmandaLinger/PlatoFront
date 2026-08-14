e987b537867fe2a4610aaf8bd340048df7e39942
amanda <amandamarqueslinger@gmail.com>
2026-08-13 21:36:33 -0300

    ajustes

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {ItemHome} from '../../components/item-home/item-home';
+import {RouterLink} from '@angular/router';
+import {FooterComponent} from '../../components/footer-component/footer-component';
+import {Configuracao} from '../../components/configuracao/configuracao';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [
+    ItemHome,
+    RouterLink,
+    FooterComponent,
+    Configuracao
+  ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
