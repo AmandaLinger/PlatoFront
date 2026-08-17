43ff6993a7928afca533458292e3e99e2c548d4e
amanda <amandamarqueslinger@gmail.com>
2026-08-17 11:51:47 -0300

    ajustes na página de configuração

--- src/app/pages/configuracoes-page/configuracoes-page.ts
+++ src/app/pages/configuracoes-page/configuracoes-page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { ItemHome } from '../../components/item-home/item-home';
+import {BtnBack} from '../../components/btn-back/btn-back';
 
 @Component({
   selector: 'app-configuracoes-page',
-  imports: [],
+  imports: [ItemHome, BtnBack],
   templateUrl: './configuracoes-page.html',
   styleUrl: './configuracoes-page.scss',
 })
-export class ConfiguracoesPage {
-
-}
+export class ConfiguracoesPage {}
